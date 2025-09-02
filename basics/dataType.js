3362e515c3ffdd884a14f5fd17881abd8af9ec03
Sidharth Kumar Mourya <91594412+Sidhukumar00029@users.noreply.github.com>
2025-09-02 11:49:02 +0000

    dataType Done

---
+++ basics/dataType.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all JS as Newer Version
+
+/*
+number
+bigint
+string
+boolean
+null => standalone value, It is a object
+undefined => value not defined yet now
+symbol => unique
+object
+*/
