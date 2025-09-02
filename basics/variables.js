e86954a2d761c5833927f6c8d5f4bd1b00537bb1
Sidharth Kumar Mourya <91594412+Sidhukumar00029@users.noreply.github.com>
2025-09-02 11:33:44 +0000

    devcontainer config done

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountID =12345
+let accountEmail = "sid12345@google.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+
+accountId = 2
+let account;
+
+console.log(accountId);
+console.table([accountID, accountEmail, accountPassword, accountCity, account])
